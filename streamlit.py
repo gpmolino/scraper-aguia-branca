07af9e93763ad036a25b97958ebfe810e4266cf7
gpmolino <gpmolino27@gmail.com>
2022-12-28 23:29:39 -0300

    remove sidebar streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import services
 
-st.sidebar.title('Aguia Branca Scrapper')
-origin = st.sidebar.selectbox('Origem', ['linhares-es', 'vitoria-es', 'colatina-es', 'vicosa-mg'])
-destination = st.sidebar.selectbox('Destino', ['linhares-es', 'vitoria-es', 'colatina-es', 'vicosa-mg'])
+st.title('Aguia Branca Scrapper')
+origin = st.selectbox('Origem', ['linhares-es', 'vitoria-es', 'colatina-es', 'vicosa-mg'])
+destination = st.selectbox('Destino', ['linhares-es', 'vitoria-es', 'colatina-es', 'vicosa-mg'])
 
 st.table(services.aguia_branca_trips(origin, destination))
